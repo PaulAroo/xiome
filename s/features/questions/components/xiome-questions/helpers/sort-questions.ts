3c4f8d04d544f0c17c30436e27ade22186ccb75a
Paul_Aroo <timmypaul2006@gmail.com>
2021-10-19 22:51:01 +0100

    update sort-questions algorithm

--- s/features/questions/components/xiome-questions/helpers/sort-questions.ts
+++ s/features/questions/components/xiome-questions/helpers/sort-questions.ts
@@ -5,6 +5,7 @@ export function sortQuestions(questions: Question[], myUserId?: string) {
 
 	const myQuestions: Question[] = []
 	const otherQuestions: Question[] = []
+	const defaultTimeFactor: number = 86400000
 
 	for (const question of questions) {
 		const isMine = myUserId && question.authorUserId === myUserId
@@ -14,18 +15,27 @@ export function sortQuestions(questions: Question[], myUserId?: string) {
 			otherQuestions.push(question)
 	}
 
+	function valueOfVotesInTime(votes: number, timeFactor: number) {
+		const value = votes*timeFactor
+		return Math.log(value) / Math.LN10;
+	}
+
 	function sortingFunction(a: Question, b: Question) {
 		const promoteA = -1
 		const promoteB = 1
+		
+		const scoreA = a.timePosted + valueOfVotesInTime(a.likes, 1*defaultTimeFactor) - valueOfVotesInTime(a.reports, 2*defaultTimeFactor)
+		const scoreB = b.timePosted + valueOfVotesInTime(b.likes, 1*defaultTimeFactor) - valueOfVotesInTime(b.reports, 2*defaultTimeFactor)
 
-		if (a.likes > b.likes) return promoteA
-		if (a.likes < b.likes) return promoteB
 
-		if (a.reports < b.reports) return promoteA
-		if (a.reports > b.reports) return promoteB
+		if (scoreA > scoreB) return promoteA
+		if (scoreB < scoreA) return promoteB
 
-		if (a.timePosted > b.timePosted) return promoteA
-		if (a.timePosted < b.timePosted) return promoteB
+		// if (a.reports < b.reports) return promoteA
+		// if (a.reports > b.reports) return promoteB
+
+		// if (a.timePosted > b.timePosted) return promoteA
+		// if (a.timePosted < b.timePosted) return promoteB
 
 		return 0
 	}
